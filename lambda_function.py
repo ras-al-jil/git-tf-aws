f229eabfd68ad13b506e1aff8329689a4611196f
Rajil Paloth <30567462+ras-al-jil@users.noreply.github.com>
2026-02-11 17:20:12 +0530

    Implement basic Lambda function to receive SQS messages

---
+++ lambda_function.py
@@ -0,0 +1,21 @@
+#Recieve message.py
+import json
+import boto3
+from datetime import datetime
+
+client = boto3.client('sqs')
+
+def lambda_handler(event, context):
+    # TODO implement
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Message received at ", current_time)
+    print("=========================")
+    response = client.receive_message(QueueUrl='https://sqs.us-east-1.amazonaws.com/661279896448/CaseTestQueue',MaxNumberOfMessages=10)
+    print(response)
+    print("=========================")
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
